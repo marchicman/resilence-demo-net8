4936c9b01a83ac8269a1a82ef15018ffcffec864
Manlio Marchica <mmarchica@datacolor.com>
2024-05-04 19:50:30 +0200

    commented out RoutePrefix

--- Program.cs
+++ Program.cs
@@ -29,7 +29,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI(options =>
     {
         options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
-        options.RoutePrefix = string.Empty;
+        //options.RoutePrefix = string.Empty;
     });
 }
 
